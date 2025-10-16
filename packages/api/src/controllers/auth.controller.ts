385d5450043686e7164bb44ef7e4705d16422c5e
lenidita120 <lenidita120@gmail.com>
2025-10-16 11:45:24 +0700

    Update auth.controller.ts

--- packages/api/src/controllers/auth.controller.ts
+++ packages/api/src/controllers/auth.controller.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from 'express';
+import { createUser } from '../services/user.service'; // <-- IMPORT INI
 
 export const registerUser = async (req: Request, res: Response) => {
-  // Nanti logika registrasi ditaruh di sini
-  res.status(201).json({ message: 'User registered successfully', data: req.body });
+  try {
+    const user = await createUser(req.body);
+    res.status(201).json({ message: 'User registered successfully', data: user });
+  } catch (error: any) {
+    // Nanti kita buat error handling yg lebih bagus
+    res.status(500).json({ message: 'Error registering user', error: error.message });
+  }
 };
 
 export const loginUser = async (req: Request, res: Response) => {
